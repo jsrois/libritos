c5cb183ad39f3aac23de8ca66e58f6fe3179c939
jsrois <javier.sanchez.rois@gmail.com>
2020-07-02 23:27:02 +0200

    Springboot gradle project

---
+++ src/main/kotlin/net/jsrois/libritos/LibritosApplication.kt
@@ -0,0 +1,11 @@
+package net.jsrois.libritos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LibritosApplication
+
+fun main(args: Array<String>) {
+	runApplication<LibritosApplication>(*args)
+}
