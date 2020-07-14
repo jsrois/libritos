d7909cc4239de30a54e6a13ad1c20537339ed88d
jsrois <javier.sanchez.rois@gmail.com>
2020-07-14 23:23:21 +0200

    Added React with typescript

---
+++ web-client/src/components/render.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export const App = (): JSX.Element => <div>
+    <h1 id="page-title">Libritos</h1>
+</div>;
+
+
--- web-client/webpack.config.js
+++ web-client/webpack.config.js
@@ -2,18 +2,18 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
-    entry: path.join(__dirname, 'src/app.ts'),
+    entry: path.join(__dirname, 'src/app.tsx'),
     output: {
         filename: "bundle.js",
         path: path.resolve(__dirname,'build')
     },
     resolve: {
-        extensions: [".ts"]
+        extensions: [".tsx", ".ts", ".js"]
     },
     module: {
         rules: [
             {
-                test: /\.ts$/,
+                test: /\.tsx?$/,
                 loader: "ts-loader",
                 exclude: /node_modules/
             }
