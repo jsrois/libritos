1e57fcbd5dece781534a6a09830039df32225c92
jsrois <javier.sanchez.rois@gmail.com>
2020-07-23 18:37:31 +0200

    Read books in the frontend from Phony Api

--- web-client/src/components/Library.tsx
+++ web-client/src/components/Library.tsx
@@ -1,3 +1,35 @@
 import * as React from "react";
+import {useContext, useEffect, useState} from "react";
 
-export const Library = (): JSX.Element => <section></section>;
+class Book {
+    private readonly _title: string;
+    private readonly _author: string;
+
+    constructor(title: string, author: string) {
+        this._title = title;
+        this._author = author;
+    }
+
+    asString =  (): string => `${this._title} by ${this._author}`;
+}
+
+const phonyApi = {
+    getBooks: (): Promise<Array<Book>> => Promise.resolve([
+        new Book("Das Kapital", "Karl Marx"),
+        new Book("Women, Race & Class", "Angela Y.Davis")]
+    )
+}
+
+export const Library = (): JSX.Element => {
+    const [books, setBooks] = useState(Array<Book>())
+
+    useEffect( () => {
+        phonyApi.getBooks().then(books => setBooks(books))
+    }, [])
+
+    return <section>
+        <ul>
+            { books.map( (book,i) => <li key={i}>{book.asString()}</li>)}
+        </ul>
+    </section>
+};
