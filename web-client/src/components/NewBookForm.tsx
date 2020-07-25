84a309be529f165a6f29e5ec5c20b122fab924fe
jsrois <javier.sanchez.rois@gmail.com>
2020-07-25 12:33:28 +0200

    Validate title and author not empty

--- web-client/src/components/NewBookForm.tsx
+++ web-client/src/components/NewBookForm.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import {Book} from "../repository/Book";
 import {useInput} from "../hooks/useInput";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import {Redirect} from "react-router";
 
 export const NewBookForm = (props: { addBook: (book: Book) => void }) => {
-
-    const { value: title, bind: bindTitle } = useInput("");
-    const { value: author, bind: bindAuthor } = useInput("");
-    const [ redirect, setRedirect ] = useState(false);
+    const {value: title, bind: bindTitle} = useInput("");
+    const {value: author, bind: bindAuthor} = useInput("");
+    const [redirect, setRedirect] = useState(false);
 
     const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -16,20 +15,26 @@ export const NewBookForm = (props: { addBook: (book: Book) => void }) => {
         setRedirect(true);
     };
 
+    const [disabled, setDisabled] = useState(true);
+    useEffect(() => {
+        setDisabled(title == "" || author == "")
+    }, [title, author]);
+
+
     if (redirect) {
         return <Redirect to="/"/>
     }
 
     return <form className="Form" onSubmit={onSubmit}>
         <label className="Form__label" htmlFor="new-book-name">Book Name</label>
-        <input className="Form__textBox" id="new-book-name" type="text" {...bindTitle} />
+        <input className={"Form__textBox"} id="new-book-name" type="text" {...bindTitle} />
         <label className="Form__label" htmlFor="new-book-author">Book Author</label>
         <input className="Form__textBox" id="new-book-author" type="text" {...bindAuthor}/>
         <div className="Form__check">
             <input type="checkbox"/>
             <label htmlFor="new-book-read">Already Read</label>
         </div>
-        <input className="Form__submitButton" type="submit" value="Add Book"/>
+        <input disabled={disabled} className="Form__submitButton" type="submit" value="Add Book"/>
 
     </form>;
 }
